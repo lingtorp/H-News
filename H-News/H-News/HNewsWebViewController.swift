6cfb4be8a2cf8e684e3d79b5308045b13eac2688
Alexander Lingtorp <AlexanderLingtorp@Alexanders-MacBook-Pro.local>
2015-12-08 12:13:01 +0100

    Added guards, adde KVO to observe title and loading change.
    TODO: Add a activity spinner when the page is loading.

--- H-News/H-News/HNewsWebViewController.swift
+++ H-News/H-News/HNewsWebViewController.swift
@@ -16,36 +16,66 @@ class HNewsWebViewController: UIViewController {
     /// The url to load
     var url: NSURL? {
         didSet {
-            loadWebViewWith(url!)
+            guard let url = url else { return }
+            loadWebViewWith(url)
         }
     }
     
     /// The data to load
     var data: NSData? {
         didSet {
-            loadWebViewWith(data!)
+            guard let data = data else { return }
+            loadWebViewWith(data)
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         webView.frame = view.bounds
+        webView.addObserver(self, forKeyPath: "loading", options: .New, context: nil)
+        webView.addObserver(self, forKeyPath: "title", options: .New, context: nil)
         view.addSubview(webView)
     }
     
+    deinit {
+        webView.removeObserver(self, forKeyPath: "loading")
+        webView.removeObserver(self, forKeyPath: "title")
+    }
+    
     private func loadWebViewWith(data: NSData) {
-        guard let string = NSString(data: data, encoding: NSASCIIStringEncoding) as? String else { return }
-        webView.loadHTMLString(string, baseURL: nil)
-        title = url?.host
+        guard let html = NSString(data: data, encoding: NSASCIIStringEncoding) as? String else { return }
+        webView.loadHTMLString(html, baseURL: nil)
     }
     
     private func loadWebViewWith(url: NSURL) {
         webView.loadRequest(NSURLRequest(URL: url))
-        title = url.host
     }
     
     @IBAction func onShare(sender: UIBarButtonItem) {
-        let shareSheet = UIActivityViewController(activityItems: [url!], applicationActivities: nil)
+        guard let url = url else { return }
+        let shareSheet = UIActivityViewController(activityItems: [url], applicationActivities: nil)
         presentViewController(shareSheet, animated: true, completion: nil)
+    }
+    
+    // MARK: - WKWebView KVO
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        guard let _ = object as? WKWebView else {return}
+        guard let keyPath = keyPath else {return}
+        guard let change = change else {return}
+        switch keyPath {
+            case "loading":
+                if let loading = change[NSKeyValueChangeNewKey] as? Bool {
+                    if loading {
+                        // activity.startAnimating()
+                    } else {
+                        // activity.stopAnimating()
+                    }
+                }
+            case "title":
+                if let newTitle = change[NSKeyValueChangeNewKey] as? String {
+                    title = newTitle
+                }
+            default: break
+        }
     }
 }
