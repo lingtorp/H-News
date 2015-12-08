01f8a8cc2059edc1e8e76b2798f23ca4be7f0a4b
Alexander Lingtorp <AlexanderLingtorp@Alexanders-MacBook-Pro.local>
2015-12-08 12:13:12 +0100

    Added Setting V.C

---
+++ H-News/H-News/HNewsSettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HNewsSettingsViewController.swift
+//  H-News
+//
+//  Created by Alexander Lingtorp on 08/12/15.
+//  Copyright © 2015 Lingtorp. All rights reserved.
+//
+
+import UIKit
+
+class HNewsSettingsViewController: UITableViewController {
+ 
+    @IBAction func onClose(sender: NSObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
